d8a94ae49cc6d36f47bb766a1f5fbe92f89547e7
Xander Juneau <xanderjuneau@gmail.com>
2022-11-19 17:00:20 -0500

    refactored projects to take in json data to render project entries

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,40 +1,49 @@
-import React from 'react';
-import "./projects.css"
-
+import React from "react";
+import "./projects.css";
+import projects from '../../data/projects.json'
 function Projects() {
-    const handleOnMouseMove = (e) => {
-        const {currentTarget: target} = e;
 
-        const rect = target.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
+  const handleOnMouseMove = (e) => {
+    const { currentTarget: target } = e;
 
-        target.style.setProperty("--mouse-x", `${x}px`)
-        target.style.setProperty("--mouse-y", `${y}px`)
-    }
-    
+    const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
 
-    return ( 
-    <div className='project-container'>
-        <div className='project-header'></div>
-        <div className='project-entry'>
-            <div onMouseMove={(e)=>handleOnMouseMove(e)} className='project-card'></div>
-            <div className='project-description'></div>
-        </div>
-        <div className='project-entry'>
-            <div onMouseMove={(e)=>handleOnMouseMove(e)} className='project-card'></div>
-            <div className='project-description'></div>
-        </div>
-        <div className='project-entry'>
-            <div onMouseMove={(e)=>handleOnMouseMove(e)} className='project-card'></div>
-            <div className='project-description'></div>
-        </div>
-        <div className='project-entry'>
-            <div onMouseMove={(e)=>handleOnMouseMove(e)} className='project-card'></div>
-            <div className='project-description'></div>
-        </div>
-    </div> 
-    );
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+
+  return (
+    <div className="project-container">
+      <div className="project-header"></div>
+      {projects.map((project) => {
+        const {picture, name, description, link, linkIcon} = project
+        return (
+          <div className="project-entry">
+            <div
+              onMouseMove={(e) => handleOnMouseMove(e)}
+              className="project-card"
+            >
+              <div className="project-card-row">
+                <div className="project-card-image">
+                    <img src={`${picture}`} />
+                </div>
+                <div className="project-card-name">{name}</div>
+              </div>
+              <div className="project-card-row">
+                <div className="project-card-description">{description}</div>
+              </div>
+              <div className="project-card-row-links">
+                <div className="project-card-links"></div>
+              </div>
+            </div>
+            <div className="project-description"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Projects;
