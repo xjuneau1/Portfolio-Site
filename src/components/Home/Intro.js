ecd9081b478e879ccf55d3544f3e8920a7fc7d0b
Xander Juneau <xanderjuneau@gmail.com>
2022-11-21 18:50:32 -0500

    added ninja style

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import "./intro.css"
-const profile = process.env.PUBLIC_URL + "/images/profile.JPG"
+import { animate } from "framer-motion";
+import React from "react";
+import "./intro.css";
+const profile = process.env.PUBLIC_URL + "/images/profile.JPG";
 
 function Intro() {
-    return ( 
-        <div className="intro-container">
-        <div className="greeting-container">
-          <h1>Hi, I'm Xander!</h1>
-          <p>
-            Full-stack Developer and CSS Ninja, proficient in Javascript, React.js, Node.js,
-            and PostgreSQL. I build eye-pleasing responsive websites, REStful APIs and SQL Databases.
-          </p>
-        </div>
-        <div className="profile-picture-container">
-          <img
-            className="profile-picture"
-            src={profile}
-            alt="picture of the site creator"
-          />
-        </div>
+  
+  return (
+    <div className="intro-container">
+      <div className="greeting-container">
+        <h1>Hi, I'm Xander!</h1>
+        <p>
+          Full-stack Developer and{" "}
+          <span className="magic">
+            <span className="magic-text">CSS Ninja</span>
+          </span>
+          , proficient in Javascript, React.js, Node.js, and PostgreSQL. I build
+          eye-pleasing responsive websites, REStful APIs and SQL Databases.
+        </p>
       </div>
-     );
+      <div className="profile-picture-container">
+        <img
+          className="profile-picture"
+          src={profile}
+          alt="picture of the site creator"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Intro;
